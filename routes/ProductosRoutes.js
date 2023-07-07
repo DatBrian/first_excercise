0f293b4aaf299f2b38b355e1bf2276158d76934c
DatBrian <vikard.2005@hotmail.com>
2023-07-07 00:04:36 -0500

    feat: :poop: Endpoint get configurado pero falta arreglar la logica
    Se implementó la consulta get pero falta arreglar la lógica para que sume las cantidades y implementar funcionalidades extra que estan comentadas para el optimo uso

--- routes/ProductosRoutes.js
+++ routes/ProductosRoutes.js
@@ -23,11 +23,7 @@ ProductosRouter.use((req, res, next) => {
 ProductosRouter.get('/', async (req, res) => {
     try {
         const query = `
-            SELECT p.*, SUM(i.cantidad) AS Total
-            FROM productos p
-            JOIN inventarios i ON p.id = i.id_producto
-            GROUP BY p.id
-            ORDER BY Total DESC
+            SELECT DISTINCT id_producto, (SELECT SUM(cantidad) FROM inventarios WHERE id_producto = t.id_producto) AS total FROM inventarios AS t WHERE id_producto IN(SELECT id_producto FROM inventarios GROUP BY id_producto HAVING COUNT(*) > 0) ORDER BY total;
         `;
         const [rows] = await connection.query(query);
         res.json(rows);
@@ -36,6 +32,38 @@ ProductosRouter.get('/', async (req, res) => {
         res.status(500).json({ error: 'Ocurrió un error al obtener los productos' });
 
     }
+});
+
+//? Agregar un nuevo producto y añadirlo a una bodega por defecto
+ProductosRouter.post('/', async (req, res) => {
+    try {
+        //* Recoger los datos de la petición y definir la fecha actual
+        const { nombre, descripcion, estado, created_by, update_by, deleted_at, cantidad_inicial } = req.body;
+        const currentDate = new Date().toISOString().slice(0, 19).replace('T', ' ');
+
+        // //* Verificar si existe un producto con el mismo nombre
+        // const searchProductQuery = 'SELECT id FROM productos WHERE nombre = ?';
+        // const [commonProduct] = await connection.query(searchProductQuery, [nombre]);
+
+        // //* Definir la bodega por defecto y el id del producto dependiendo de si existe o no
+        // const productId = (commonProduct.length > 0) ? await insertExistentProduct(commonProduct[0])
+        //     : await insertNewProduct();
+        const bodega = 11;
+
+        const insertProductQuery = 'INSERT INTO productos (nombre, descripcion, estado, created_by, update_by, created_at, updated_at, deleted_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+        await connection.query(insertProductQuery, [nombre, descripcion, estado, created_by, update_by, currentDate, currentDate, deleted_at]);
+
+        //* Insertar registro en la tabla inventarios
+        const insertInventoryQuery = 'INSERT INTO inventarios (id_bodega, id_producto, cantidad, created_by, update_by, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?)';
+        await connection.query(insertInventoryQuery, [bodega, productId, cantidad_inicial, created_by, update_by, currentDate, currentDate])
+
+        res.json({ "Message": 'Producto creado y agregado al inventario en la bodega por defecto' });
+    } catch (error) {
+        console.error('Error al agregar los productos:', error);
+        res.status(500).json({ error: 'Ocurrió un error al agregar los productos' });
+
+    }
+
 });
 
 export default ProductosRouter;
